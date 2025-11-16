5176c5a5fa339098685d203135a2f23b16a01106
anurag panuhar <165539474+prahans@users.noreply.github.com>
2025-11-16 16:24:32 +0545

    assignment

--- matrices.java
+++ matrices.java
@@ -29,21 +29,21 @@ public class matrices {
         }
     }
 
-    // public static int diagonalSum(int arr[][]){ // brute force
-    //     int sum = 0;
-    //     for(int i=0; i<arr.length; i++){
-    //         for(int j=0; j<arr[0].length; j++){
-    //             if(i == j){
-    //                 sum += arr[i][j];
-    //             }else if(i + j == arr.length-1){
-    //                 sum += arr[i][j];
-    //             }
-    //         }
-    //     }
-    //     return sum;
-    // }
+    public static int diagonalSum(int arr[][]){ // brute force
+        int sum = 0;
+        for(int i=0; i<arr.length; i++){
+            for(int j=0; j<arr[0].length; j++){
+                if(i == j){
+                    sum += arr[i][j];
+                }else if(i + j == arr.length-1){
+                    sum += arr[i][j];
+                }
+            }
+        }
+        return sum;
+    }
 
-    public static int diagonalSum(int arr[][]){ //optimized
+    public static int diagonalSum1(int arr[][]){ //optimized
         int sum = 0; 
         for(int i=0; i<arr.length; i++){
             sum += arr[i][i];
@@ -55,7 +55,106 @@ public class matrices {
         return sum;
     }
 
-    public static void main(String args[]){
+    //searching or finding target on 2D array or matrix
+    public static void findTargetIndex(int arr[][], int target){ // brute froce O(n^2)
+        for(int i=0; i<arr.length; i++){
+            for(int j=0; j<arr[0].length; j++){
+                if(arr[i][j] == target){
+                    System.out.println("(" + i + ", " + j + ")");
+                }
+            }
+        }
+    }
+
+    public static boolean searchRowWise(int arr[][], int target){ // O(n log n)
+        for(int i=0; i<arr.length; i++){
+            int l = 0;
+            int r = arr[0].length;
+            while(l <= r){
+                int mid = l + (r - l)/2;
+                if(arr[i][mid] == target){
+                    System.out.print("(" + i + ", " + mid + ")");
+                    return true;
+                }else if(arr[i][mid] > target){
+                    r = mid -1;
+                }else{
+                    l = mid +1;
+                }
+            }
+        }
+        System.out.print("target doesn't found.");
+        return false;
+    }
+
+    public static boolean stairCaseSearch(int arr[][], int target){
+        int row = 0 , col = arr[0].length -1;
+        while(row < arr.length && col >= 0){
+            if(arr[row][col] == target){
+                System.out.print("(" + row + ", " + col + ")");
+                return true;
+            }else if(arr[row][col] > target){
+                col--;
+            }else{
+                row++;
+            }
+        }
+        System.out.print("target does't found.");
+        return false;
+    }
+
+    //Question 1 :Print the number of 7’s that are inthe 2d array.
+    public static int qs1(int arr[][]){
+        int countOf7 = 0;
+        for(int i=0; i<arr.length; i++){
+            for(int j=0; j<arr[0].length; j++){
+                if(arr[i][j] == 7){
+                    countOf7++;
+                }
+            }
+        }
+        return countOf7;
+    }
+
+    //Question 2 :Print out the sum of the numbers inthe second row of the “nums” array
+    public static int qs2(int arr[][]){
+        int sum = 0;
+        for(int i=0; i<arr[0].length; i++){
+            sum += arr[1][i];
+        }
+        return sum;
+    }
+
+    //Question 3 :Write a program to FindTranspose of a Matrix.What is Transpose?
+    public static void findAndPrintTranspose(int matrix[][]){
+        int row = matrix.length , col = matrix[0].length;
+        System.out.println("original matrix");
+        //printing orignal matrix
+        for(int i=0; i<row; i++){
+            for(int j=0; j<col; j++){
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+        //transpose the matrix
+        int [][] transpose = new int[col][row];
+        for(int i=0; i<col; i++){
+            for(int j=0; j<row; j++){
+                transpose[i][j] = matrix[j][i];
+            }
+        }
+        
+        System.out.println("transpose matrix");
+        //print transpose
+        for(int i=0; i<col; i++){
+            for(int j=0; j<row; j++){
+                System.out.print(transpose[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String args[]){ 
         Scanner sc = new Scanner(System.in);
         int matrix[][] = {
             {1, 2, 3, 4},
@@ -70,6 +169,33 @@ public class matrices {
             {3, 4, 5},
             {6, 7, 8}
         };
-        System.out.println(diagonalSum(matrix2));
+        //sorted matrix in bothwise row & column respectively.
+        int matrix3[][] = {
+            {10, 20, 30, 40},
+            {15, 25, 35, 45},
+            {27, 29, 37, 48},
+            {32, 33, 39, 50}
+        };
+
+        int arrQs1[][] = {
+            {4, 7, 8},
+            {8, 8, 7}
+        };
+
+        int[][] nums = { {1,4,9},{11,4,3},{2,2,3} };
+
+        int [][] matrix4 =  { 
+            {2,3,7},
+            {5,6,7}
+        };
+
+        // System.out.println(diagonalSum(matrix2));
+        // findTargetIndex(matrix2, 4);
+        // searchRowWise(matrix3, 20);
+        // System.out.println(stairCaseSearch(matrix3, 33));
+        // System.out.println(qs1(arrQs1));
+        // System.out.println("sum = " + qs2(nums));
+        findAndPrintTranspose(matrix4);
+
     }
 }
