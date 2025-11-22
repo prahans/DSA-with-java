6bb3a372caa384631f2c1412332427b15abeb93a
anurag panuhar <165539474+prahans@users.noreply.github.com>
2025-11-22 16:52:44 +0545

    new file

--- RecursionBasic.java
+++ RecursionBasic.java
@@ -17,9 +17,113 @@ public class RecursionBasic {
         System.out.print(n+" ");
     }
 
+    public static int fact(int n){
+        if(n == 0){
+            return 1;
+        }
+        int fmn = fact(n -1);
+        int fn = n * fmn;
+        return fn;
+    }
+
+    public static int sum(int n){
+        if(n == 1){
+            return 1;
+        }
+        int smn = sum(n-1);
+        int sumN = n + smn;
+        return sumN;
+    }
+
+    //calculate nth term in fibonacci
+    public static int fib(int n){
+        if(n == 0 || n == 1){
+            return n;
+        }
+        int fnm1 = fib(n-1);
+        int fnm2 = fib(n-2);
+        int fn = fnm1 + fnm2;
+        return fn;
+    }
+
+    //check given array is sorted or not
+    public static boolean isSorted(int arr[], int i){
+        if(i == arr.length -1){
+            return true;
+        }
+
+        if(arr[i] > arr[i + 1]){
+            return false;
+        }
+
+        return isSorted(arr, i+1);
+    }
+
+    public static int firstOccurence(int arr[], int key, int i){
+        if(i == arr.length){
+            return -1;
+        }
+        if(key == arr[i]){
+            return i;
+        }
+        return firstOccurence(arr, key, i+1);
+    }
+
+    public static int lastOccurence(int arr[], int key, int i){
+        if(i == -1){
+            return i;
+        }
+        if(arr[i] == key){
+            return i;
+        }
+        return lastOccurence(arr, key, i-1);
+    }
+
+    public static int lastOccurence2(int arr[], int key, int i){
+        if(i == arr.length){
+            return -1;
+        }
+        int isFound = lastOccurence2(arr, key, i+1);
+        if(isFound == -1 && arr[i] == key){
+            return i;
+        }
+        return isFound;
+    }
+
+    public static int power(int x, int n){
+        if(n == 0){
+            return 1;
+        }
+        return x * power(x,n-1);
+    }
+
+    public static int optimizedPower(int a, int n){ // O(log n)
+        if(n == 0){
+            return 1;
+        }
+
+        int halfPower = optimizedPower(a, n/2);
+        int halfPowerSq = halfPower * halfPower;
+
+        //n is odd
+        if(n%2 != 0){
+            halfPowerSq = halfPowerSq * a;
+        }
+
+        return halfPowerSq;
+    }
+
     public static void main(String args []){
-        printDec(10);
-        System.out.println();
-        printInc(10);
+        System.out.println(optimizedPower(2,5));
+        // System.out.println(power(2,2));
+        // int arr[] = {8, 3, 6, 9, 5, 10, 2, 5, 3};
+        // System.out.println(firstOccurence(arr, 5, 0));
+        // System.out.println(lastOccurence2(arr, 5, 0));
+        // printDec(10);
+        // System.out.println();
+        // printInc(10);
+        // System.out.println(fact(5));
+        // System.out.println(sum(5));
+        // System.out.println(fib(25));
     }
 }
