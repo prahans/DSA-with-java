12a11c7a04ccf24df1444320c6b9fd7367807099
anurag panuhar <165539474+prahans@users.noreply.github.com>
2025-12-05 09:00:22 +0545

    adding new files

--- RecursionBasic.java
+++ RecursionBasic.java
@@ -26,14 +26,14 @@ public class RecursionBasic {
         return fn;
     }
 
-    public static int sum(int n){
-        if(n == 1){
-            return 1;
-        }
-        int smn = sum(n-1);
-        int sumN = n + smn;
-        return sumN;
-    }
+    // public static int sum(int n){
+    //     if(n == 1){
+    //         return 1;
+    //     }
+    //     int smn = sum(n-1);
+    //     int sumN = n + smn;
+    //     return sumN;
+    // }
 
     //calculate nth term in fibonacci
     public static int fib(int n){
@@ -113,8 +113,47 @@ public class RecursionBasic {
         return halfPowerSq;
     }
 
+    public static int tilingProblem(int n){
+        //base case
+        if(n == 0 || n == 1){
+            return 1;
+        }
+
+        //kaam
+        //vertical choice
+        int fnm1 = tilingProblem(n-1);
+
+        //horizontal choice
+        int fnm2 = tilingProblem(n-2);
+
+        return fnm1 + fnm2;
+    }
+
+    public static void removeDuplicate(String str, int idx, StringBuilder newStr, boolean map[]){
+        if(idx == str.length()){
+            System.out.println(newStr);
+            return;
+        }
+        char currChar = str.charAt(idx);
+        if(map[currChar - 'a'] == true){
+            removeDuplicate(str, idx+1, newStr, map);
+        }else{
+            map[currChar - 'a'] = true;
+            removeDuplicate(str, idx+1, newStr.append(currChar), map);
+        }
+    }
+
+    public static int sum(int n){
+        if(n == 1){
+            return n;
+        }
+
+        return n + sum(n-1);
+    }
+
     public static void main(String args []){
-        System.out.println(optimizedPower(2,5));
+        // System.out.println(tilingProblem(5));
+        // System.out.println(optimizedPower(2,5));
         // System.out.println(power(2,2));
         // int arr[] = {8, 3, 6, 9, 5, 10, 2, 5, 3};
         // System.out.println(firstOccurence(arr, 5, 0));
@@ -125,5 +164,10 @@ public class RecursionBasic {
         // System.out.println(fact(5));
         // System.out.println(sum(5));
         // System.out.println(fib(25));
+
+        // String str = "appnnacollege";
+        // removeDuplicate(str, 0, new StringBuilder(""), new boolean [26]);
+
+        System.out.println(sum(20));
     }
 }
--- javaBasics.java
+++ javaBasics.java
@@ -80,7 +80,129 @@ public class javaBasics {
         }
     }
 
+    // recursion qs 1
+    public static void dec(int n){
+        if(n == 1){
+            System.out.print(n + " ");
+            return;
+        }
+        System.out.print(n + " ");
+        dec(n-1);
+    }
+
+    // recursion qs 2
+    public static void inc(int n){
+        if(n == 1){
+            System.out.print(n + " ");
+            return;
+        }
+        inc(n -1);
+        System.out.print(n + " ");
+    }
+
+    // recursion qs 3
+    public static int fact(int n){
+        if(n == 0){
+            return 1;
+        }
+        return n * fact(n-1);
+    }
+
+    // recursion qs 4
+    public static int sum(int n){
+        if(n == 1){
+            return 1;
+        }
+        return n + sum(n-1);
+    }
+
+    // recusion qs 5
+    public static int fib(int n){
+        if(n == 0 || n == 1){
+            return n;
+        }
+        return fib(n-1) + fib(n -2);
+    }
+
+    // recursion qs 6
+    public static boolean isSorted(int arr[], int i){
+        if(i == arr.length-1){
+            return true;
+        }
+        if(arr[i] > arr[i+1]){
+            return false;
+        }
+        return isSorted(arr,i+1);
+    }
+
+    // recursion qs 7
+    public static int firstOccurence(int arr[], int key, int i){
+        if(i == arr.length){
+            return -1;
+        }
+        if(arr[i] == key){
+            return i;
+        }
+        return firstOccurence(arr, key, i+1);
+    }
+
+    // recursion qs 8
+    public static int lastOccurence(int arr[], int key, int i){
+        if(i == -1){
+            return -1;
+        }
+        if(arr[i] == key){
+            return i;
+        }
+        return lastOccurence(arr, key, i-1);
+    }
+
+    // recursion qs 9
+    public static int lastOccurence2(int arr[], int key, int i){
+        if(i == arr.length){
+            return -1;
+        }
+        int isFound = lastOccurence2(arr, key, i+1);
+        if(isFound == -1 && arr[i] == key){
+            return i;
+        }
+        return isFound;
+    }
+
+    // recurison qs 10
+    public static int power(int a, int n){
+        if(n == 0){
+            return 1;
+        }
+        return a * power(a,n-1);
+    }
+
+    // recurison qs 11
+    public static int optimizedPower(int a, int n){
+        if(n == 0){
+            return 1;
+        }
+        int halfPower = optimizedPower(a, n/2);
+        int halfPowerSq = halfPower * halfPower;
+        // n is odd
+        if(n%2 != 0){
+            halfPowerSq = halfPowerSq * a;
+        }
+        return halfPowerSq;
+    }
+
+    public static void hollowRec(int n){
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=n; j++){
+                if(i == 1 || j == 1){
+                    System.out.print("* ");
+                }System.out.print(" ");
+            }
+            System.out.println();
+        }
+    }
+
     public static void main(String args[]){
-        printMyName(9);
+        hollowRec(5);
     }
 }
